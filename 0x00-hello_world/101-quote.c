9f56d163ef3d4f8c8eb335248082e12da81cd69b
Mub2023 <mubatta@gmail.com>
2023-02-16 18:38:43 -0800

    task 8  Write a C program that prints exactly and that piece of art is useful Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO,"and that piece of art is useful\" - Dora Korpar, 2015-10-19".strlen());
+	write(STDOUT_FILENO,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
 	return (1);
 }
