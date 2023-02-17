4989abd6ce9e333c817efe2fac2493bb3d58b5b8
Mub2023 <mubatta@gmail.com>
2023-02-17 12:57:20 -0800

    task 3 prints the alphabet in lowercase, and then in uppercase, followed by a new line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int sa;
+	int bg;
+	for (sa = 'a', sa <= 'z', sa++)
+		putchar(sa);
+	putchar('\n');
+	for (bg = 'A', bg <= 'Z', bg++)
+		putchar(bg);
+	putchar('\n');
+	return (0);
+}
