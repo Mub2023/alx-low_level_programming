627b50928bd68188425e3dc2e93e039fe84dabec
Mub2023 <mubatta@gmail.com>
2023-02-17 18:39:23 -0800

    task 8 a program that prints all the numbers of base 16 in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,8 +11,8 @@ int main(void)
 	int hex;
 	int ex;
 
-	for (hex = '0'; hex <= 'f'; hex++)
-		putchar(hex);
+	for (hex = '0'; hex <= '9'; hex++)
+		putchar((hex % 10)+ '0');
 	for (ex = 'a'; ex <= 'f'; ex++)
 		putchar(ex);
 	putchar('\n');
