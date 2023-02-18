ebc5d7d0b900e8f61eaf5467c19c3d94d95124d3
Mub2023 <mubatta@gmail.com>
2023-02-18 11:32:05 -0800

    task 8  Write a C program that prints exactly and that piece of art is useful Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 /*main - Entry point
  *description :print and that piece of art is useful - Dora Korpar, 2015-10-19
  * Return: Always 1 (Success)
