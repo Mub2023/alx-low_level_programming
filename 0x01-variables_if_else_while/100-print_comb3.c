1860472455cf9da6a3b490734ff1966ba710c8a6
Mub2023 <mubatta@gmail.com>
2023-02-17 19:18:45 -0800

    task 10  a program that prints all possible different combinations of two digits.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int i, w;
+	
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (w =0 ; w <= '9'; w++)
+		{
+			if (i != w)
+			{
+				putchar(i);
+				putchar(w);
+
+				if (i == '8' && w =='9')
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar ('\n');
+
+	return (0);
+}
