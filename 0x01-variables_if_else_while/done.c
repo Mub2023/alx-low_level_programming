13f00a7e2365b84eef41964fe757e296d148cd08
Mub2023 <mubatta@gmail.com>
2023-02-19 14:01:11 -0800

    done

---
+++ 0x01-variables_if_else_while/done.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * return :Always (0) success
+ **/
+int main(void)
+{
+	int m, u, b;
+
+	{
+	    for (m = 0; m <= 100 ;m++)
+		    printf("#finally done!!\n");
+	    for (u = 0; u <= 100 ;u++)
+			   printf("#do_hard_things!\n");
+	    for (b = 0; b <= 100 ; b++)
+		    printf("Mub2023!\n");
+	}
+	    return (0);
+}
