fc85b81df2b45720ff3961bdcbde83b15e8dadf8
Mub2023 <mubatta@gmail.com>
2023-02-21 13:45:22 -0800

    task 7 print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+/*
+ * file : 7-print_last_digit.c
+ * Auth :mubarak musad atta
+ */
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit of a number.
+ * @m: The number in question.
+ * Return : The value of the last digit,
+ */
+int print_last_digit(int c)
+{
+	int m;
+	
+	m = c % 10;
+		return (m);
+}
