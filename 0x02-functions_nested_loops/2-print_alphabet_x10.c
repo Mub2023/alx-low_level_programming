4acb3f0465e1f596b87d6a487d64a97217707804
Mub2023 <mubatta@gmail.com>
2023-02-21 09:15:13 -0800

    task 2 a function that prints the alphabet x10, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,16 +2,14 @@
 /**
  * main - Entry point
  * Return: Always 0 (Success)
- * */
+ **/
 void print_alphabet_x10(void)
 {
 	int count;
 	char b;
 
-		for (count = 0; count <= 9; count++)
-		{
+		for (count = 0; count <= 9; count++)	
 	for (b = 'a';b <= 'z'; b++)
-		_putchar('b');
+		_putchar(b);
 	_putchar('\n');
-		}
 }
