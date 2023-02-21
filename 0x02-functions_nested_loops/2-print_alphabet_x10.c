87718e03b4a47cf09805d3b33fb29a9a11d7f9e5
Mub2023 <mubatta@gmail.com>
2023-02-21 08:01:30 -0800

    task 2 a function that prints the alphabet x10, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * */
+void print_alphabet_x10(void);
+{
+	char a;
+	int count = 0
+
+		while (count++ <= 9)
+		{
+	for (a = 'a';a <= 'z'; a++)
+		_putchar('a');
+		}
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,5 @@
  **/
 char _putchar(char a);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 #endif
