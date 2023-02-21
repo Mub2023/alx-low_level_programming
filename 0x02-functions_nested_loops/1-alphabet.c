916ce51338877f595ea373ad5f9161a3aa5868b9
Mub2023 <mubatta@gmail.com>
2023-02-21 07:46:09 -0800

    task 1 a function that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
 #include "main.h"
 /**
  * main - Entry point
- * description :'print_alphabet - prints the alphabet in lowercase, followed by a new line.'
+ * description :'print_alphabet'
  * Return: Always 0 (Success)
  **/
 void print_alphabet(void)
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,8 @@
  * Auth : mubarak musad atta
  * Desc: Header file containing declarations for all functions
  * used in the 0x02-functions_nested_loops directory.
+ * main - Entry point
+ * Return: Always 0 (Success)
  **/
 char _putchar(char a);
 void print_alphabet(void);
