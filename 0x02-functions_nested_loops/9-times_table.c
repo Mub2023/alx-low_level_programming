7cb69d9cd7ce1516efc8975cdac05f158972d8dd
Mub2023 <mubatta@gmail.com>
2023-02-21 15:37:51 -0800

    task 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,16 +11,16 @@ void times_table(void)
 	int hi;
 
 	for (hi = '0'; hi <= '9'; hi++)
-		_putchar(hi * 0);
+		_putchar(hi * '0');
 	_putchar(',');
 	_putchar(' ');
-	_putchar(hi * 1);
+	_putchar(hi * '1');
 		_putchar(',');
 		_putchar(' ');
-	_putchar(hi * 2);
+	_putchar(hi * '2');
 	_putchar(',');
 	_putchar(' ');
-	_putchar(hi * 3);
+	_putchar(hi * '3');
 	_putchar(',');
 	_putchar(' ');
 	_putchar(hi * 4);
