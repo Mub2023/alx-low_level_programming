907cce31ca4ee650b84cb02d7d7f5c12ffb7530a
Mub2023 <mubatta@gmail.com>
2023-02-21 06:20:29 -0800

    task 0 a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+#ifndef main_h
+#define main_h
+/*
+ * file : main.h
+ * Auth : mubarak musad atta
+ * Desc: Header file containing declarations for all functions
+ * used in the 0x02-functions_nested_loops directory.
+ **/
+
+char ma [8] = "_putchar"
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
