df5323e154fc6c91e8a5652a91ac396ff83fc83f
Mub2023 <mubatta@gmail.com>
2023-02-21 10:12:28 -0800

    task 2 a function that prints the alphabet x10, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@
  * Header file containing declarations for all functions
  * used in the 0x02-functions_nested_loops directory
  **/
-int _putchar(char a);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
