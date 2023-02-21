64a0ad6f22a379d60efb7ecb88ac678316d6da13
Mub2023 <mubatta@gmail.com>
2023-02-21 13:07:17 -0800

    task 6 computes the absolute value of an integer.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+/*
+ * file : 6-abs.c
+ * Auth : mubarak musad atta
+ */
+
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ * 
+ * Return: The absolute value of the integer.
+ */
+int _abs(int c)
+{
+	int w;
+
+	w = abs(c);
+	_putchar(w);
+	return (0);
+}
