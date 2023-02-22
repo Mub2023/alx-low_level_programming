46ea61d55c3bf6362cdea66f53452c0bba47a102
Mub2023 <mubatta@gmail.com>
2023-02-22 11:44:19 -0800

    task 0 Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,9 @@
-/*
- * file : 0-main.c
- * Auth : mubarak musad atta
- */
 #include "main.h"
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
+ * Auth : mubarak musad atta
+ * i don't like betty
  */
 int main(void)
 {
