f45317a01209d8f59a77dc2ba4645c1e7ddece33
Mub2023 <mubatta@gmail.com>
2023-02-21 17:58:43 -0800

    task 11 Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,16 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-	while (n++ < 98)
+	while (n < 98)
+		n++;
 	       _putchar(n);
 	_putchar(',');
 _putchar(' ');
 	}
 	else (n > 98)
 	{
-		while (n-- > 98)
+		while (n > 98)
+			n--;
 			_putchar(n);
 		_putchar(',');
 		_putchar(' ');
