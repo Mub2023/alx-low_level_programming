6d45426352c0392a429097a9dc501398de31d0d7
Mub2023 <mubatta@gmail.com>
2023-02-21 17:03:33 -0800

    task 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,9 +26,9 @@ void times_table(void)
 	}
 	else
 	{
-		_putchar((bye / 10 + 0));
+		_putchar((bye / 10 + '0'));
 
-		_putchar((bye % 10 + 0));
+		_putchar((bye % 10 + '0'));
 	}
 	}
 	_putchar('\n');
