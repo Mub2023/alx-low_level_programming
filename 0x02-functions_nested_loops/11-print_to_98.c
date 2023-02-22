b4a5133b9aebc67a2276e20155d01fff0c557662
Mub2023 <mubatta@gmail.com>
2023-02-21 17:50:01 -0800

    task 11 Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,12 +6,42 @@
 /**
  * print_to_98 - Prints all natural numbers from input to 98,
  * in order separated by a comma followed by a space.
- * @b: The number to begin counting at.
+ * @n: The number to begin counting at.
  */
-void print_to_98(int b)
+void print_to_98(int n)
 {
-	for (b = '0'; b <= '98'; b++)
-	       _putchar(b);
+	if (n == 0)
+	{
+	for (n = 0; n <= 98; n++)
+	       _putchar(n);
 	_putchar(',');
 _putchar(' ');
+	}
+	else if (n == 98)
+	{
+		for (n = 98; n <= 111; n++)
+			_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+		else if (n == 111)
+		{
+			for (n = 111; n <= 98; n--)
+				_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+		}
+		else if (n == 81)
+		{
+			for (n = 81; n <= 98; n++)
+				_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+		}
+		else if (n == -10)
+		{
+			for (n = -10; n <= 98; n++)
+				_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+		}
 }
