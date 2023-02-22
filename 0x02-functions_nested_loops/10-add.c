49a57c43269cdb32f0d6eb05ddb899e61ccb589f
Mub2023 <mubatta@gmail.com>
2023-02-21 17:08:42 -0800

    task 10 a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+/*
+ * file :10-add.c
+ * Auth : mubarak musad atta
+ */
+#include "main.h"
+/**
+ * a + b adds two integers and returns the result
+ * Retrun : sum the result
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
