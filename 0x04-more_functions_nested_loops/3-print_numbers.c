73c88d2e96279e1fca041cf3709675a624bc1eac
Mub2023 <mubatta@gmail.com>
2023-02-23 15:37:32 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers -  a function that prints the numbers,
+ * from 0 to 9, followed by a new line.
+ * return: 0 always (Success)
+ * Auth : mubarak musad atta
+ */
+void print_numbers(void)
+{
+	int lee
+
+	for (lee >= '0'; lee <= '9'; lee++)
+	{
+		_putchar(lee);
+	}
+	_putchar('\n');
+	return (0);
+}
