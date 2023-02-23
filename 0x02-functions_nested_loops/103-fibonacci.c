6278e8465b56af43f8a17fd32894f01e1b577055
Mub2023 <mubatta@gmail.com>
2023-02-22 18:08:25 -0800

    task 15 fix

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+/**
+ * file name : 103-fibonacci.c
+ * athu : muarak musad atta
+ */
+#include <stdio.h>
+/**
+ * main  - fibonacci numbers
+ *
+ * Return: Always 0
+ **/
+int main(void)
+{
+	unsigned long s, l, p;
+	float y;
+
+	s = 0
+		l = 1
+		while (1)
+		{
+			p = s + l;
+			if (p > 4000000)
+				break;
+
+			if ((p % 2) == 0)
+				y += p;
+
+			s = l;
+			l = p;
+		}
+	printf("%.0f\n", p);
+
+	return (0);
+}
