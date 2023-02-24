155a7713171adf4ab554fe83218b40bba47e4a83
Mub2023 <mubatta@gmail.com>
2023-02-23 17:49:55 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - a program that prints the numbers from 1 to 100,
+ *  followed by a new line.
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int me
+
+	for (me = 1; me <= 100; me++)
+	{
+		if ((me % 3 == 0) && (me % 5 == 0))
+		prinf("Fizz Buzz\n");
+		else if (me % 3 == 0)
+			printf("Fizz\n");
+		else if (me % 5 == 0)
+			printf("Buzz\n");
+		else
+			printf("%d\n", me);
+	}
+}
