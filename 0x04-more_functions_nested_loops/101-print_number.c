59762ba275e2aba5b43807d9477937398c92bb7c
Mub2023 <mubatta@gmail.com>
2023-02-23 18:45:18 -0800

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: the prints numbers
+ * Return: Always 0 (Success)
+ */
+void print_number(int n)
+{
+	unsigned int mm = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		mm = -mm;
+	}
+	if ((mm / 10) > 0)
+		print_number(mm / 10);
+
+	_putchar((num % 10) + '0');
+}
