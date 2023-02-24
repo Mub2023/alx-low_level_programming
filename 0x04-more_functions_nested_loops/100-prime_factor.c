e0948ed1509fe1b156bc81dffbe7323fa2aa8b38
Mub2023 <mubatta@gmail.com>
2023-02-23 18:29:39 -0800

    task 3

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - a program that finds and prints the largest prime factor
+ *  of the number 612852475143, followed by a new line.
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int mu;
+	long num = 612852475143;
+
+	for (mu = (int) sqrt(num); mu > 2; mu++)
+	{
+		if (num % mu == 0)
+		{
+			printf("%d\n", c);
+			break;
+		}
+	}
+	return (0);
+}
