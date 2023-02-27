a2cc3d3a9456386744fa50fefa35202dd2ba447d
Mub2023 <mubatta@gmail.com>
2023-02-27 14:38:19 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+ * Auth :mubarak musad atta
+ */
+/**
+ * _puts - a function that prints a string, followed by a new line, to stdout
+ * @str: the string of input
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	int st = 0;
+
+	while (str[st] =! 0)
+	{
+		_putchar(str[st]);
+		st++;
+	}
+	_putchar('\n');
+}
