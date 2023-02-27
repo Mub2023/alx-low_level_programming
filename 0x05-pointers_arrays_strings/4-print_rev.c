d664cfc5664e0ee1f01e8f9a3f008a087f95fe4b
Mub2023 <mubatta@gmail.com>
2023-02-27 15:02:03 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*
+ * Auth : mubarak musadatta
+ */
+/**
+ * print_rev - a function that prints a string, in reverse, followed by a new line.
+ * @s: is the pointer to the string
+ * Retrun: Always 0 (Success)
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	while (c)
+		_putchar(s[--]c);
+
+	_putchar('\n');
+}
