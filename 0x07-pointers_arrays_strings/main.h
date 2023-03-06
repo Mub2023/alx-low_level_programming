a967f7f5f674a0e6eb5574abf655ce89583defd1
Mub2023 <mubatta@gmail.com>
2023-03-06 05:06:38 -0800

    task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
  * file: main.h
  * Auth: mubarak musad atta
  */
+#include <stddef.h>
 int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
