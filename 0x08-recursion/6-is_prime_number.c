ffbf33f72246b00cd603843292265b9dacd2e547
Mub2023 <mubatta@gmail.com>
2023-03-08 10:31:13 -0800

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,10 +7,17 @@
  */
 int is_prime_number(int n)
 {
+	int m = n / 2;
+
 	if (n <= 1)
 		return (0);
-	else
-		if (n % (is_prime_number(n - 1)) == 0)
+	else if (n % m == 0)
+	{
 			return (1);
-	return (1);
+	}
+	else
+	{
+		m = m - 1;
+		is_prime_number(n);
+	}
 }
