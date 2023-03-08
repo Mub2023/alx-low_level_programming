eb98f0fcdd7cbe3c013eb27d96a9f5ebab90409a
Mub2023 <mubatta@gmail.com>
2023-03-08 09:39:37 -0800

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,10 +5,8 @@
  * Return: 1 if the number is divisble .
  *  0 if the number is not divisble
  */
-int is_prime_number(int n)
+int is_prime_number(int n, dv)
 {
-	int dv;
-
 	if (n % dv == 0)
 		return (0);
 
