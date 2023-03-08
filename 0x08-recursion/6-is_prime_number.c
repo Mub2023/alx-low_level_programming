bcf0f215791c58a2d6471485c439abc08222c744
Mub2023 <mubatta@gmail.com>
2023-03-08 11:06:35 -0800

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,38 @@
 #include "main.h"
+int checker(int number, int dv);
+int is_prime_number(int n);
+/**
+ * checker - checks the number is divisble
+ * @number: the input number
+ * @dv: the divisor
+ * Return: 0 if divisble, 1 if not divisble
+ */
+int checker(int number, int dv)
+{
+	if (number % dv == 0)
+		return (0);
+
+	if (dv == number / 2)
+		return (1);
+
+	return (checker(number, dv + 1));
+}
+
 /**
  * is_prime_number - checks if a number is prime
  * @n: the number will be checked
- * Return: 1 if the number is prime .
- *  0 if the number is not prime
+ * Return: 1 if the number is prime
+ * 0 if the number is not prime
  */
 int is_prime_number(int n)
 {
-	int m = n / 2;
+	int dv = 2;
 
 	if (n <= 1)
 		return (0);
-	else if (n % m == 0)
-	{
-			return (1);
-	}
-	else
-	{
-		m = m - 1;
-		is_prime_number(n);
-	}
+
+	if (n >= 2 && n <= 3)
+		return (1);
+
+	return (checker(n, dv));
 }
