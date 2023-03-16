db4b10019864603ed7697566c1a258bd07e0a8ff
Mub2023 <mubatta@gmail.com>
2023-03-16 16:29:34 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char **argv)
 {
-	int n1, n2, result;
+	int num1, num2, result;
 
 	if (argc != 3)
 	{
@@ -18,9 +18,9 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-		result = n1 * n2;
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
 		printf("%d\n", result);
 		return (0);
 	}
