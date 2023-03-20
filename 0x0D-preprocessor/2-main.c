039f7214e34996e3488a052227ecf595484499ca
Mub2023 <mubatta@gmail.com>
2023-03-20 05:58:50 -0700

    task 3

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - prints the name file it was compiled from, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
