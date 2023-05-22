e96788efe8de53f8b7e3af63e40afccc70da95ce
Mub2023 <mubatta@gmail.com>
2023-05-22 11:02:06 -0700

    M_U B_2 0_2 3_Task 0_2 T_Y

---
+++ 0x18-dynamic_libraries/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - a function that multiplies two integers.
+ * @a: first number.
+ * @b: second number.
+ * Return: 0 always
+ */
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
---
+++ 0x18-dynamic_libraries/3-op_functions.c
@@ -0,0 +1,53 @@
+
+#include <stdio.h>
+
+/**
+ * add - add two integer
+ * @a: input number
+ * @b: input integer
+ * Return: the result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtract two integer
+ * @a: input integer
+ * @b: input integer
+ * Return: the result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multbliy two integer
+ * @a: input integer
+ * @b: input integer
+ * Return: the result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - division two integer
+ * @a: input integer
+ * @b: input integer
+ * Return: the result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - remainder of the division of two integer
+ * @a: input integer
+ * @b: input integer
+ * Return: the result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
