48dda732553be56c88c3ad823d6434f228324509
Mub2023 <mubatta@gmail.com>
2023-05-22 13:50:15 -0700

    M_U B_2 0_2 3_Task 0_3 T_Y

--- 0x18-dynamic_libraries/my_file.c
+++ 0x18-dynamic_libraries/my_file.c
@@ -1,22 +1,25 @@
 #include <unistd.h>
 #include <string.h>
-
-int rand()
+/**
+ * random - ganret random number for gm file
+ * Return: Always 0
+ */
+int random(void)
 {
-	static int ct = -1;
+	static int x = -1;
 
-	ct++;
-	if (ct == 0)
-		return 8;
-	if (ct == 1)
-		return 8;
-	if (ct == 2)
-		return 7;
-	if (ct == 3)
-		return 9;
-	if (ct == 4)
-		return 23;
-	if (ct == 5)
-		return 74;
-	return ct * ct % 30000;
+	x++;
+	if (x == 0)
+		return (8);
+	if (x == 1)
+		return (8);
+	if (x == 2)
+		return (7);
+	if (x == 3)
+		return (9);
+	if (x == 4)
+		return (23);
+	if (x == 5)
+		return (74);
+	return (x * x % 30000);
 }
