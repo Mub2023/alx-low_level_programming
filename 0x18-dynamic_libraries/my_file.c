580416ae494d2effaac7203d65173faf461394d5
Mub2023 <mubatta@gmail.com>
2023-05-22 13:32:36 -0700

    M_U B_2 0_2 3_Task 0_3 T_Y

--- 0x18-dynamic_libraries/my_file.c
+++ 0x18-dynamic_libraries/my_file.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
 #include <unistd.h>
-/**
- * random - fixed numbers instead of random ones
- * Return: fixed numbers instead of random ones
- */
-int random(void)
-{
-	static int x;
-	int n[] = {8, 8, 7, 9, 23, 74};
+#include <string.h>
 
-	if (x > 5)
-		x = 0;
-	return (n[x++]);
+int rand()
+{
+	static int ct = -1;
+
+	ct++;
+	if (ct == 0)
+		return 8;
+	if (ct == 1)
+		return 8;
+	if (ct == 2)
+		return 7;
+	if (ct == 3)
+		return 9;
+	if (ct == 4)
+		return 23;
+	if (ct == 5)
+		return 74;
+	return ct * ct % 30000;
 }
