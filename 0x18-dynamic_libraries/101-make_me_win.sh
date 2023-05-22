e93490f793626b3307dd2dc2f52401a579edf3ec
Mub2023 <mubatta@gmail.com>
2023-05-22 13:44:22 -0700

    M_U B_2 0_2 3_Task 0_3 T_Y

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/libmyfile.so https://github.com/Mub2023/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmyfile.so
+wget -P /tmp https://github.com/Mub2023/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmyfile.so
 export LD_PRELOAD=/tmp/libmyfile.so
