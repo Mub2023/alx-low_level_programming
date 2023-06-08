14b54c20e86c84dfaf2568c9acc09a3c51304398
mub2023 <mubatta@gmail.com>
2023-06-08 22:46:02 +0200

    M_U B_2 0_2 3_Task 0_3 FIX T_Y

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -31,7 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 	}
 	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
+	if (node->key == NULL)
 	{
 		free(copy);
 		return (0);
